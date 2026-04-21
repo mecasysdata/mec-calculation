1eb0ded22f33e9693d54e25a59a583c69dded7f9
mecasysdata <mecasysdata@gmail.com>
2026-04-21 10:31:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,26 +4,6 @@ import pandas as pd
 import requests
 import re
 
-# Vytvoríme dva stĺpce: prvý pre logo (šírka 1) a druhý pre text (šírka 3)
-# Pomer 1:3 zabezpečí, že logo nebude príliš dominantné
-col1, col2 = st.columns([1, 3])
-
-with col1:
-    st.image("logo.png")
-
-with col2:
-    st.title("MEC Calculation")
-    st.write("Vitajte vo vašej aplikácii na výpočet cien!")
-
-st.divider() # Pridá jemnú deliacu čiaru
-
-#zaciatok aplikacie
-
-import streamlit as st
-import pandas as pd
-import requests
-import re
-
 # --- KONFIGURÁCIA STRÁNKY ---
 st.set_page_config(layout="wide") # Roztiahne aplikáciu na celú šírku, aby stĺpce nevyzerali stiesnene
 
