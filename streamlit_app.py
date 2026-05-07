6a28dbcafabb5650d4c6250d661c6052d23e3bc5
mecasysdata <mecasysdata@gmail.com>
2026-05-07 10:04:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,13 @@ with col_title:
     st.title("MEC Calculation")
 
 st.divider()
+
+# Načítanie dát z Google Sheet - zákazník
+sheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSuHQWbpryWNerWr8aKKheHbzTPhXI6lS7YH1sL5zwFIIzLfpTZz47acY_ua2e_fVqEcfxMBe5wnjue/pub?gid=0&single=true&output=csv"
+df = pd.read_csv(sheet_url)
+
+# Príprava zoznamu zákazníkov (zoradený abecedne)
+zoznam_zakaznikov = sorted(df['zakaznik'].unique())
 # --- 3. RIADOK S ATRIBÚTMI ---
 # Vytvoríme si stĺpce, aby sme mohli dávať prvky vedľa seba
 col1, col2, col3, col4 = st.columns(4)
@@ -31,3 +38,31 @@ with col2:
     # - label: "Označenie CP" (názov, ktorý uvidí používateľ)
     # - value: "" (necháme prázdne, aby používateľ mohol písať)
     ponuka = st.text_input("Označenie CP")
+# --- LOGIKA PRE ZOZNAM ZÁKAZNÍKOV ---
+# Pridáme možnosť pre nového zákazníka na začiatok zoznamu
+moznosti_zakaznikov = ["+ Pridať nového zákazníka"] + zoznam_zakaznikov
+
+with col3:
+    vyber = st.selectbox("Názov Zákazníka", moznosti_zakaznikov)
+
+# --- LOGIKA ROZHODOVANIA ---
+if vyber == "+ Pridať nového zákazníka":
+    # Režim: Nový zákazník
+    with col3:
+        zakaznik = st.text_input("Zadajte meno nového zákazníka")
+    
+    with col4:
+        krajina_hodnota = st.text_input("Krajina Zákazníka (manuálne)")
+    
+    lojalita = 0.5  # Automaticky nastavená hodnota
+
+else:
+    # Režim: Existujúci zákazník (pôvodná logika)
+    data_zakaznika = df[df['zakaznik'] == vyber].iloc[0]
+    zakaznik = vyber
+    krajina_hodnota = str(data_zakaznika['krajina'])
+    lojalita = float(data_zakaznika['lojalita'])
+
+    with col4:
+        # Pole zostáva uzamknuté (disabled), keďže krajinu poznáme
+        st.text_input("Krajina Zákazníka", value=krajina_hodnota, disabled=True)
