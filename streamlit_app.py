40813cbe51e586f8c8472d8ea65ba783f2eb4fcc
mecasysdata <mecasysdata@gmail.com>
2026-05-18 08:33:21 +0200

    Fix import statement for FPDF library

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 import datetime
 import re
 import math
-from fpdf2 import FPDF
+from fpdf import FPDF
 
 # --- 1. NASTAVENIA ---
 st.set_page_config(layout="wide", page_title="MEC Calculation")
